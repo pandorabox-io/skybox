ba1b030d325ec393ae2dcdfad52c329d4d4675a0
Thomas Rudin <thomas@rudin.io>
2018-08-22 09:40:57 +0200

    always day in space

--- init.lua
+++ init.lua
@@ -17,6 +17,7 @@ register_skybox({
 	miny = 5001,
 	maxy = 6000,
 	gravity = 0.1654,
+	always_day = true,
 	textures = {"space_sky.png","space_sky.png","space_sky.png","space_sky.png","space_sky.png","space_sky.png"}
 })
 
@@ -26,6 +27,8 @@ register_skybox({
 	miny = 6001,
 	maxy = 10000,
 	gravity = 0.1,
+	alyways_day = true,
+	fly = true,
 	textures = {
 		"sky_pos_z.png",
 		"sky_neg_z.png^[transformR180",
@@ -63,12 +66,16 @@ local update_skybox = function(player)
 				player:set_sky({r=0, g=0, b=0},"skybox", box.textures)
 				player:set_clouds({density=0,speed=0})
 				player:set_physics_override({gravity=box.gravity})
+				if box.always_day then
+					player:override_day_night_ratio(1)
+				end
 				return
 			end
 		end
 	end
 
 	-- no match, return to default
+	player:override_day_night_ratio(nil)
 	skybox_cache[name] = ""
 	player:set_sky({r=0, g=0, b=0},"regular",{})
 	player:set_clouds({
