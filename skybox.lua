c28397f36744ec287b14df7ebc10405263ac9877
Thomas Rudin <thomas@rudin.io>
2019-05-01 19:39:08 +0200

    remove skybox and textures

--- skybox.lua
+++ skybox.lua
@@ -16,55 +16,6 @@ skybox.register = function(def)
 	table.insert(skybox_list, def)
 end
 
--- TODO: move to own mod
-skybox.register({
-	name = "space",
-	miny = 1000,
-	maxy = 5000,
-	gravity = 0.8,
-	textures = {"space_sky.png","space_sky2.png","space_sky.png","space_sky.png","space_sky.png","space_sky.png"}
-})
-
--- moon
--- TODO: move to own mod
-skybox.register({
-	name = "moon",
-	miny = 5001,
-	maxy = 6000,
-	gravity = 0.1654,
-	always_day = true,
-	textures = {"space_sky.png","space_sky.png","space_sky.png","space_sky.png","space_sky.png","space_sky.png"}
-})
-
-
--- speep space
--- TODO: move to own mod
-skybox.register({
-	-- https://github.com/Ezhh/other_worlds/blob/master/skybox.lua
-	name = "deepspace",
-	miny = 6001,
-	maxy = 10999,
-	gravity = 0.1,
-	always_day = true,
-	fly = true,
-	textures = {
-		"sky_pos_z.png",
-		"sky_neg_z.png^[transformR180",
-		"sky_neg_y.png^[transformR270",
-		"sky_pos_y.png^[transformR270",
-		"sky_pos_x.png^[transformR270",
-		"sky_neg_x.png^[transformR90"
-	}
-})
-
-skybox.register({
-	name = "earth_cave",
-	miny = -32000,
-	maxy = -50,
-	gravity = 1,
-	sky_type = "plain",
-	sky_color = {r=0, g=0, b=0}
-})
 
 
 local timer = 0
