908d4899340eed6e2eb35bc93f687fa6a7966493
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-09-23 20:24:51 +0200

    move default_skybox to own module

--- init.lua
+++ init.lua
@@ -8,5 +8,6 @@ skybox = {
 
 local MP = minetest.get_modpath("skybox")
 dofile(MP.."/register.lua")
+dofile(MP.."/defaults.lua")
 dofile(MP.."/skybox.lua")
 dofile(MP.."/compat.lua")
--- skybox.lua
+++ skybox.lua
@@ -52,41 +52,6 @@ function skybox.get_skybox_for_player(player)
 	return newbox
 end
 
--- sets the default skybox for the player
-function skybox.set_default_skybox(player)
-	player:override_day_night_ratio(nil)
-
-	-- https://github.com/minetest/minetest/blob/4a3728d828fa8896b49e80fdc68f5d7647bf45b7/src/skyparams.h#L75-L88
-	player:set_sky({
-		clouds = true,
-		type = "regular",
-		sky_color = {
-			day_sky = "#61b5f5",
-			day_horizon = "#90d3f6",
-			dawn_sky = "#b4bafa",
-			dawn_horizon = "#bac1f0",
-			night_sky = "#006bff",
-			night_horizon = "#4090ff",
-			indoors = "#646464",
-			fog_tint_type = "default",
-		}
-	})
-
-	player:set_moon({ visible = true })
-	player:set_sun({ visible = true, sunrise_visible = true })
-	player:set_stars({ visible = true })
-
-	player:set_clouds({
-		thickness=16,
-		color={r=243, g=214, b=255, a=229},
-		ambient={r=0, g=0, b=0, a=255},
-		density=0.4,
-		height=200,
-		speed={y=-2,x=-1}
-	})
-
-end
-
 function skybox.update_skybox(player)
 	local pos = player:get_pos()
 	local name = player:get_player_name()
